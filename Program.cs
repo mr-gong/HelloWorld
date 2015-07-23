b9a71ac304d9afc590e793552a407497aa84f311
mr-gong <506688268@qq.com>
2015-07-23 18:26:35 +0800

    22

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace HelloWorld
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+namespace HelloWorld
 {
 	internal class Program
 	{
 		private static void Main ( string[] args )
 		{
+			Console.WriteLine ( "HelloWorld!" );
+			Console.ReadKey();
 		}
 	}
 }
